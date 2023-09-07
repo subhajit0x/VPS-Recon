e6bdfb05134d616ccc8cf05480044b17456102b8
Subhajit Saha <108721112+subhajit0x@users.noreply.github.com>
2023-09-07 11:25:37 +0530

    Update easy-recon.sh

--- easy-recon.sh
+++ easy-recon.sh
@@ -1,34 +1,66 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 echo "Enter your Target - >"
 
 read DomainName
 
-mkdir autorecon-recon-results
-cd ~/autorecon-recon-results/
-pwd
-sleep 2
+mkdir -p $DomainName
+cd $DomainName
+echo "Starting recon on $DomainName"
 
-subfinder -d $DomainName -silent | httpx -silent | nuclei -t ../nuclei-templates/ -o nuclei-subs.txt
+# Subdomain enumeration
+echo "Running Subfinder"
+subfinder -d $DomainName -o subdomains.txt
+sleep 10;
 
+# Subdomain enumeration with crt.sh
+echo "========== Fetching Subdomains from crt.sh =========="
+curl -s "https://crt.sh/?q=%25.$DomainName&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee -a subdomains.txt && echo "[+] crt.sh fetch completed."
+sleep 5
 
-echo $DomainName | subfinder -silent | httpx -silent | hakrawler > hakrawler-subs.txt
+echo "Running Httpx"
+httpx -l subdomains.txt  -o httpx.txt
+echo " "
+echo "============== subdomains.txt & httpx.txt saved & done ================="
+sleep 10
+# Shodan scans
+echo "================ Shodan Scan ============="
+shodan init 3GX1LChUx5PBe9PeYMTFyFiOH42u2aeV
 
-sleep 2
+shodan domain $DomainName > shodan.txt
 
-amass enum -passive -norecursive -noalts -d $DomainName -o amass-results.txt
-assetfinder $DomainName | httprobe > assetfinder-live.txt
-#add more tools to get all subdomains
+sleep 5;
+echo "=================== Shodan Scan Done =================="
+# Asset Discovery
+echo "=========== Assetfider Scan running ================"
+assetfinder --subs-only $DomainName | tee -a assetfinder.txt
+echo "============== Assetfinder Scan Done =============="
+sleep 5;
+# Amass for DNS Enumeration
+echo "============== Amass Scan running ================"
+amass enum -d $DomainName -o amass.txt
+echo "=============== Amass Scan Done ================="
+sleep 3
+echo "================ Waybackurls Starting ================"
+echo $DomainName | waybackurls | tee -a waybackurls.txt
+echo "=============== Waybackurls Scan Done ================="
+# Gather Javascript Files
+cat httpx.txt | getJS --complete | tee -a jsfiles.txt
+sleep 3;
+# Directory bruteforcing with GoBuster
+gobuster dir -u https://$DomainName -w /Users/subhajit/Desktop/recon/SecLists/Discovery/Web-Content/common.txt -b 301 -o gobuster.txt
+sleep 10;
+# Use hakrawler for crawling 
+echo $DomainName | hakrawler | tee -a hakrawler.txt
 
+# Use Nuclei for template-based scanning
+nuclei -l httpx.txt -t /Users/subhajit/Desktop/recon/nuclei-templates -o nuclei.txt
 cat *.txt | sort -u > all.txt 
 
-sleep 2
-echo " CHECKING LIVE DOMAINS >>>>>"
-cat all.txt | httpx -o live-subs.txt
-sleep 2
+# Use Arjun to discover hidden HTTP parameters
+arjun -i httpx.txt --stable -oT arjun.txt 
+sleep 10;
+# Cleanup duplicates
+sort -u all.txt -o all.txt
 
-
-#snap install feroxbuster
-# ln /path/to/the/wordlist ~/snap/feroxbuster/common or cp /path/to/the/wordlist ~/snap/feroxbuster/common
-
-cat live-subs.txt | feroxbuster --stdin -e -w /snap/feroxbuster/common | tee feroxbuster.txt
+echo "Recon on $DomainName finished"
